8f62f2c87be6569fd8c6bc7e62e4080927e2cbb1
scelarek <115444760+scelarek@users.noreply.github.com>
2025-05-21 15:53:08 -0700

    create education extractor plug-in

--- cvinsight/custom_plugins/__init__.py
+++ cvinsight/custom_plugins/__init__.py
@@ -2,7 +2,16 @@
 
 from typing import List
 
+# Import all our custom plugin classes
+from .relevant_yoe_extractor.relevant_yoe_extractor import RelevantYoEExtractorPlugin
+from .education_stats_extractor.education_stats_extractor import EducationStatsExtractorPlugin  
+from .work_stats_extractor.work_stats_extractor import WorkStatsExtractorPlugin
+from .social_extractor.social_extractor import SocialExtractorPlugin
+
 # Define the list of all custom plugins
 __all__: List[str] = [
-    # "KeywordMatcherPlugin"  # Commented out to disable this plugin
+    "RelevantYoEExtractorPlugin",
+    "EducationStatsExtractorPlugin",
+    "WorkStatsExtractorPlugin", 
+    "SocialExtractorPlugin",
 ]
---
+++ cvinsight/custom_plugins/education_stats_extractor/__init__.py
@@ -0,0 +1,5 @@
+"""Education Stats Extractor Plugin."""
+
+from .education_stats_extractor import EducationStatsExtractorPlugin
+
+__all__ = ["EducationStatsExtractorPlugin"]
